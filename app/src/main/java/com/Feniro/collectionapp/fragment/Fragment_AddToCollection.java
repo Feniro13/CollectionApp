e23c119c647cb3e940dde8ea683ebcb2970dfb23
Feniro <aleks.chur13@gmail.com>
2022-04-13 11:46:18 +0300

    Database Fixed

---
+++ app/src/main/java/com/Feniro/collectionapp/fragment/Fragment_AddToCollection.java
@@ -0,0 +1,20 @@
+package com.Feniro.collectionapp.fragment;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.Feniro.collectionapp.R;
+
+public class Fragment_AddToCollection extends Fragment {
+    public Fragment_AddToCollection() {
+        super(R.layout.fragment_add_to_collection);
+
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
